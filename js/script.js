eb927b15953a7b8a687a2bf126335363a67c56aa
kpelc91 <kamilapelc01@gmail.com>
2022-06-01 11:09:26 +0200

    zadanie skończone

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 let btnDownloadDate = document.getElementById('main-button');
 
+const dataDevelop = $('<div id="dane-programisty"></div>');
+    $('body').prepend(btnDownloadDate);
+
 const downDevelop = () => {
+
     fetch('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', {
         mode: 'cors',
         method: 'GET',
     })
         .then(res => res.json())
         .then(resJSON => {
-            console.log(resJSON);
+
+        const parName = $(`<p id="name">${resJSON.imie}</p>`);
+        const parLastName = $(`<p id="last-name">${resJSON.nazwisko}</p>`);
+        const parJob = $(`<p id="job">${resJSON.zawod}</p>`);
+        const parCompany = $(`<p id="company">${resJSON.firma}</p>`);
+
+        $('body').after(parName);
+        $('body').after(parLastName);
+        $('body').after(parJob);
+        $('body').after(parCompany);
         })
+        
         .catch((error) => {
             console.error('Error:', error);
         });
@@ -16,9 +30,4 @@ const downDevelop = () => {
 
 btnDownloadDate.addEventListener('click', downDevelop);
 
-const dataDevelop = $('<div id="dane-programisty"></div>');
-$('body').prepend(btnDownloadDate);
 
-$('#main-button').click(function () {
-    
-})
